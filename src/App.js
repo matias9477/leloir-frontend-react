3aca6d3f6b79cca767aa575f69e91686339e1006
matias.turra <matias.turra@gmail.com>
2019-09-09 16:56:09 -0300

    Ahora el routeo se hace desde index.js, se elimina Navigator para no necesitar renderizar un componente que solo routee

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import 'semantic-ui-css/semantic.min.css';
 import Navigator from './Components/Navigator/Navigator';
 //import LoginForm from './Components/Login/LoginLayout';
+import LPSecretaria from './Components/LandingPages/LPSecretaria';
 
 function App(){
   return(
     <div className="App">
-      <Navigator/>
+      <LPSecretaria/>
     </div>
   );
 }
