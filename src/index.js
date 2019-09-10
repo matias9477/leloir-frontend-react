a95a909fd68852d963610225cdd605bad68f3ee9
solmalisani <solmalisani@gmail.com>
2019-09-10 10:00:42 -0300

    rutas añadidas

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import registerServiceWorker from './registerServiceWorker';
 import AuthenticatedRoute from './Services/AuthenticatedRoute';
 import LoginForm from './Components/Login/LoginLayout';
 import Tabla2 from './Components/TablaPaciente/Tabla2';
+import FormAlta from './Components/AltaPaciente/FormAlta';
+import FormConsulta from './Components/ConsultaPaciente/FormConsulta';
 
 
 ReactDOM.render(
@@ -16,6 +18,8 @@ ReactDOM.render(
         <AuthenticatedRoute exact path="/" component={App}/>
         <Route path="/login" component={LoginForm}/>
         <AuthenticatedRoute exact path="/pacientes" component={Tabla2}/>
+        <AuthenticatedRoute exact path="/pacientes/add" component={FormAlta}/>
+        <AuthenticatedRoute exact path="/pacientes/consulta/:id" component={FormConsulta}/>
         </Switch>
     </Router>
 
