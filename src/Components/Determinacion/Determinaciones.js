e40e5f8ae4ef2dce72ddefd2eafe7390dfe7c72d
Bruno_Valdeiglesias <bruno.valdeiglesias@gmail.com>
2019-09-13 18:45:40 -0300

    Hola mundo! de determinaciones

---
+++ src/Components/Determinacion/Determinaciones.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import MenuLateral from "../MenuLateral";
+
+class Determinaciones extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+
+
+    render() {
+        return (
+            <div className='union'>
+                <MenuLateral/>
+                <div className="FormAlta">
+                    <h1>Hola mundo!</h1>
+                </div>
+
+
+            </div>
+        );
+    }
+}
+
+Determinaciones.propTypes = {};
+
+export default Determinaciones;
