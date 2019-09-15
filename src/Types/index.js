1e4809da5a382f252841d94a4859b58143299358
bvaldeiglesias <bruno.valdeiglesias@gmail.com>
2019-09-15 19:21:10 -0300

    Implementacion Proptypes para manejar un solo array de pacientes

---
+++ src/Types/index.js
@@ -0,0 +1,52 @@
+// ./src/Types/index.js
+import {arrayOf, bool, number, oneOfType, string} from 'prop-types';
+import {any} from "expect";
+
+export const personaType = {
+    id: number,
+    nombre: string,
+    apellido: string,
+    tipoDocumento: string,
+    nroDocumento: string,
+    fechaNacimiento: string,
+    fechaAlta: string,
+    sexo: string,
+    nacionalidad: string,
+    obraSocial: string,
+    telefono: any,
+    mail: any,
+    historial: any,
+    bitAlta: bool
+};
+
+export const institucionType = {
+    id: number,
+    fechaAlta: string,
+    nombre: string,
+    direccion: any,
+    telefono: number,
+    mail: any,
+    historial: any,
+    fax: any,
+    bitAlta: bool
+};
+
+export const animalType = {
+    id: number,
+    fechaAlta: string,
+    nombre: string,
+    tipoAnimal: string,
+    propietario: string,
+    telefono: number,
+    mail: any,
+    historial: any,
+    bitAlta: bool
+};
+
+export const pacientesArrayType = {
+    pacientes: arrayOf(oneOfType([
+        personaType,
+        animalType,
+        institucionType
+    ]))
+};
