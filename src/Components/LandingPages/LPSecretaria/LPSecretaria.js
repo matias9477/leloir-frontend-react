3d93b7216d785b2fdfab4abccd3aeeb191f97269
matias <matias.turra@gmail.com>
2019-10-06 18:43:31 -0300

    Estilos

--- src/Components/LandingPages/LPSecretaria/LPSecretaria.js
+++ src/Components/LandingPages/LPSecretaria/LPSecretaria.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "./../../styles.css";
 import MenuOpciones from '../../MenuOpciones';
 import Afluencia from './Afluencia';
+import {Grid, Col, Row} from 'semantic-ui-react'
 import Cola from './Cola';
 
 class LPSecretaria extends Component {
@@ -13,12 +14,25 @@ class LPSecretaria extends Component {
 
     render() {
         return (
-            <div className="union" >
+            
+            <div className="union">
                 <MenuOpciones/>
-                <div className="afluence">
-                <Afluencia/>
+                <div className="avoidMenu">         
+                    <Grid>
+                        <Grid.Column width={10}>
+                            <h2>hola</h2>
+
+                        </Grid.Column>
+                        <Grid.Column width={10}>
+                           <div className="afluence">
+                               <h2>Cola de Espera</h2>
+                               <Afluencia/>
+                           </div>
+                        </Grid.Column>
+                    </Grid>
                 </div>
             </div>
+            
         );
     }
 }
