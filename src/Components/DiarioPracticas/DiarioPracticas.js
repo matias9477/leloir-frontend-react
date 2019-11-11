f8eae307da97541e0997bead1838bd57e20c3d71
bvaldeiglesias <bruno.valdeiglesias@gmail.com>
2019-11-11 19:34:56 -0300

    fix imports

--- src/Components/DiarioPracticas/DiarioPracticas.js
+++ src/Components/DiarioPracticas/DiarioPracticas.js
@@ -41,7 +41,7 @@ class DiarioPracticas extends Component {
         axios.get(urlEmitirAnalisis + idAnalisis, {responseType: 'blob',}).then((response) => {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
-            const index1 = response.headers['content-disposition'].indexOf("name=\"")+6;
+            const index1 = response.headers['content-disposition'].indexOf("name=\"") + 6;
             const index2 = response.headers['content-disposition'].indexOf("\"", 18);
             link.href = url;
             link.setAttribute('download', response.headers['content-disposition'].substring(index1, index2)); //or any other extension
