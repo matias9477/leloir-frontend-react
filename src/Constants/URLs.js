8d38be76850535deaa4ca7dd9b0e75d8a25c16f6
bvaldeiglesias <bruno.valdeiglesias@gmail.com>
2019-11-11 19:16:36 -0300

    Emision de analisis desde diario practicas

--- src/Constants/URLs.js
+++ src/Constants/URLs.js
@@ -1,27 +1,27 @@
+export const urlDocs = "/tipos_documento/all";
+export const urlObrasSoc = "/obras_sociales/all";
+export const urlPaises = "/paises/all";
+export const urlSexos = "/sexo/all";
+export const urlTiposAnimales = "/tipos-animales/all";
 
-   export const urlDocs = "/tipos_documento/all";
-   export const urlObrasSoc = "/obras_sociales/all";
-   export const urlPaises = "/paises/all";
-   export const urlSexos = "/sexo/all";
-   export const urlTiposAnimales = "/tipos-animales/all";
+export const urlObraSocial = "obras_sociales/id/";
+export const urlDeterminaciones = "/determinaciones/all";
 
-   export const urlObraSocial = "obras_sociales/id/";
-   export const urlDeterminaciones = "/determinaciones/all";
+export const urlMuestras = "/muestras/all";
+export const urlTiposMuestras = "/tipos_muestras/all";
+export const urlMuestrasAdd = '/muestras/add';
 
-   export const urlMuestras = "/muestras/all";
-   export const urlTiposMuestras = "/tipos_muestras/all";
-   export const urlMuestrasAdd = '/muestras/add';
+export const urlPacientes = "/pacientes/all";
 
-   export const urlPacientes = "/pacientes/all";
+export const urlSignUp = "/auth/signup";
+export const urlSignIn = "/auth/signin";
 
-   export const urlSignUp = "/auth/signup";
-   export const urlSignIn = "/auth/signin";
+export const urlLoggedUser = '/user/me';
 
-   export const urlLoggedUser = '/user/me';
+export const urlAnalisisPendientes = "/analisis/pendientes";
+export const urlGetAnalisis = "/analisis/id/";
+export const urlEmitirAnalisis = "/analisis/emitir-analisis/";
 
-   export const urlAnalisisPendientes = "/analisis/pendientes";
-   export const urlGetAnalisis= "/analisis/id/";
-
-   export const urlAnalisis = '/analisis/all';
-   export const urlCargarResultados = '/analisis/cargar-resultados/';
-   export const urlAprobarResultados = '/analisis/aprobar-resultados/';
+export const urlAnalisis = '/analisis/all';
+export const urlCargarResultados = '/analisis/cargar-resultados/';
+export const urlAprobarResultados = '/analisis/aprobar-resultados/';
