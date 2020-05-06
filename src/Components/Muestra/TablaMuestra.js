82458c76e9071ad0eccea93e7eb12d3f70ab255b
solmalisani <solmalisani@gmail.com>
2020-05-05 21:47:52 -0300

    incorporacion de spinner y añadi el campo paciente en la tabla y se puede filtrar por el mismo

--- src/Components/Muestra/TablaMuestra.js
+++ src/Components/Muestra/TablaMuestra.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Header, Pagination, Input, Dropdown } from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
-import { orderBy } from 'lodash'
 import { connect } from 'react-redux'
+import ClipLoader from "react-spinners/ClipLoader"
 
 import MenuOpciones from '../MenuOpciones'
 import { nroPorPagina } from "../../Constants/utils"
@@ -138,7 +138,8 @@ class TablaMuestra extends React.Component {
             const filteredMuestras = this.props.muestras.filter(m => 
                 m.idMuestra.toString().includes(filter) ||
                 ((m.tipoMuestra===undefined || m.tipoMuestra===null) ? null : m.tipoMuestra.toUpperCase().includes(filter.toUpperCase())) ||
-                m.estado.toUpperCase().includes(filter.toUpperCase())
+                m.estado.toUpperCase().includes(filter.toUpperCase()) ||
+                m.paciente.toUpperCase().includes(filter.toUpperCase())
             )
 
             if (this.state.totalCount !== filteredMuestras.length) {
@@ -154,6 +155,7 @@ class TablaMuestra extends React.Component {
 
 
     render(){
+        const { fetching } = this.props
         return(
             <div className='union'>
                 <MenuOpciones/>
@@ -165,78 +167,89 @@ class TablaMuestra extends React.Component {
                     <br></br>
                     <br></br>
                     <br></br>
+                    
+                    {fetching ? <div className='spinnerPosition'> 
+                        <ClipLoader
+                        size={50}
+                        color={"black"}
+                        />
+                        </div> :
 
-                    <div className='union'>
-                        <div className="ui icon input">
+                        <div>
 
-                            <Input value={this.state.filter} 
-                                onChange={(filter)=>
-                                    this.setState({
-                                        filter: filter.target.value
-                                    })}
-                                        
-                                placeholder='Ingrese búsqueda...' icon={{name: 'search'}} 
+                            <div className='union'>
+                                <div className="ui icon input">
+
+                                    <Input value={this.state.filter} 
+                                        onChange={(filter)=>
+                                            this.setState({
+                                                filter: filter.target.value
+                                            })}
+                                                
+                                        placeholder='Ingrese búsqueda...' icon={{name: 'search'}} 
+                                    />
+
+                                </div>
+                                {this.muestrasPerPage()}
+                            </div>
+
+                            <table className="ui single line table" >
+                                <thead className='centerAlignment'>
+                                <tr>
+                                    <th onClick={() => this.handleColumnHeaderClick("idMuestra")}  >Muestra</th>
+                                    <th onClick={() => this.handleColumnHeaderClick("paciente")} >Paciente</th>
+                                    <th onClick={() => this.handleColumnHeaderClick("analisisId")} >Analisis</th>
+                                    <th onClick={() => this.handleColumnHeaderClick("tipoMuestra")} >Tipo Muestra</th>
+                                    <th onClick={() => this.handleColumnHeaderClick("idEstado")} >Estado</th>
+                                    <th>Opciones</th>
+                                </tr>
+                                </thead>
+
+                                <tbody className='centerAlignment'>
+
+                                {(this.handleSearch(((this.state.activePage-1) * this.state.limit), (this.state.activePage * this.state.limit))).map(  (muestra, index) => (
+                                    <tr key={index} value={muestra} className={ muestra.bitActivo ? null : "listadosBaja"} >
+                                        <td data-label="Id Muestra">
+                                            {muestra.idMuestra}
+                                        </td>
+                                        <td data-label="Paciente">
+                                            {muestra.paciente}
+                                        </td>
+                                        <td data-label="Analisis">
+                                            {muestra.analisisId}
+                                        </td>
+                                        <td data-label="Tipo Muestra">
+                                            {muestra.tipoMuestra}
+                                        </td>
+                                        <td data-label="Estado">
+                                            {muestra.estado}
+                                        </td>
+                                        <td>
+                                            <Dropdown item icon='ellipsis horizontal' simple>
+                                                <Dropdown.Menu>
+                                                    <Dropdown.Item onClick={() => window.confirm(this.mensajeConfirmacion(muestra)) ? this.bitInverse(muestra): null} >
+                                                        {this.estado(muestra.bitActivo)}
+                                                    </Dropdown.Item>
+                                                    <Dropdown.Item as= {Link} to={`/muestras/id/${muestra.idMuestra}`} exact='true'>
+                                                        Ver/Modificar Proximamente
+                                                    </Dropdown.Item>
+                                                </Dropdown.Menu>
+                                            </Dropdown>
+
+                                        </td>
+                                    </tr>
+                                ))}
+
+                                </tbody>
+
+                            </table>
+                            <Pagination
+                                activePage={this.state.activePage}
+                                totalPages={this.state.filter === '' ? Math.ceil((this.props.muestras.length) / this.state.limit) : Math.ceil((this.state.totalCount) / this.state.limit)}
+                                onPageChange={this.onChangePage}
                             />
-
                         </div>
-                        {this.muestrasPerPage()}
-                    </div>
-
-                    <table className="ui single line table" >
-                        <thead className='centerAlignment'>
-                        <tr>
-                            <th onClick={() => this.handleColumnHeaderClick("idMuestra")}  >Muestra</th>
-                            <th onClick={() => this.handleColumnHeaderClick("analisisId")}  >Analisis</th>
-                            <th onClick={() => this.handleColumnHeaderClick("tipoMuestra")} >Tipo Muestra</th>
-                            <th onClick={() => this.handleColumnHeaderClick("fecha")} >Fecha</th>
-                            <th onClick={() => this.handleColumnHeaderClick("idEstado")} >Estado</th>
-                            <th>Opciones</th>
-                        </tr>
-                        </thead>
-
-                        <tbody className='centerAlignment'>
-
-                        {(this.handleSearch(((this.state.activePage-1) * this.state.limit), (this.state.activePage * this.state.limit))).map(  (muestra, index) => (
-                            <tr key={index} value={muestra} className={ muestra.bitActivo ? null : "listadosBaja"} >
-                                <td data-label="Id Muestra">
-                                    {muestra.idMuestra}
-                                </td>
-                                <td data-label="Analisis">
-                                    {muestra.analisisId}
-                                </td>
-                                <td data-label="Tipo Muestra">
-                                    {muestra.tipoMuestra}
-                                </td>
-                                <td data-label="Fecha Alta">
-                                    {muestra.fecha}
-                                </td>
-                                <td data-label="Estado">
-                                    {muestra.estado}
-                                </td>
-                                <td>
-                                    <Dropdown item icon='ellipsis horizontal' simple>
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item onClick={() => window.confirm(this.mensajeConfirmacion(muestra)) ? this.bitInverse(muestra): null} >
-                                                {this.estado(muestra.bitActivo)}
-                                            </Dropdown.Item>
-                                            <Dropdown.Item as= {Link} to={`/muestras/id/${muestra.idMuestra}`} exact='true'>
-                                                Ver/Modificar
-                                            </Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-
-                                </td>
-                            </tr>
-                        ))}
-
-                        </tbody>
-
-                    </table>
-                    <Pagination
-                        activePage={this.state.activePage}
-                        totalPages={this.state.filter === '' ? Math.ceil((this.props.muestras.length) / this.state.limit) : Math.ceil((this.state.totalCount) / this.state.limit)}
-                        onPageChange={this.onChangePage}
-                    />
+                    }
                 </div>
             </div>
         )
