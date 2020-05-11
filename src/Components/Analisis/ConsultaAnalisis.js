96463075206c15e3153e1b9fe1a84e30df2701e3
solmalisani <solmalisani@gmail.com>
2020-05-10 21:52:21 -0300

    Reorganizacion en render para q no sea tan largo cree metodos por columna y mejore el css

--- src/Components/Analisis/ConsultaAnalisis.js
+++ src/Components/Analisis/ConsultaAnalisis.js
@@ -8,12 +8,13 @@ import { connect } from 'react-redux'
 import MenuOpciones from '../MenuOpciones'
 import { getHumanDate } from '../../Services/MetodosPaciente'
 import { checkAtributo, validateRequiredCombos } from '../../Services/MetodosDeValidacion'
-import { urlMuestrasAdd, urlEmitirAnalisis } from "../../Constants/URLs"
+import { urlEmitirAnalisis } from "../../Constants/URLs"
 import ModificarResultados from '../DiarioPracticas/Modals/ModificarResultados'
 import RevisarResultados from '../DiarioPracticas/Modals/ModificarResultados'
 import { getAnalisisByIdAction } from '../../Redux/analisisDuck'
 import { getTiposMuestrasAction, addMuestraAction } from '../../Redux/muestrasDuck'
 import './../styles.css'
+import './analisisStyle.css'
 
 class ConsultaAnalisis extends Component {
     constructor(props) {
@@ -87,7 +88,7 @@ class ConsultaAnalisis extends Component {
 
     createMuestra = () => {
         return(
-            <div>
+            <div className='createMuestra'>
                 <Grid width='equal'>
                     <Grid.Column width={10}>
                         <Select
@@ -114,122 +115,147 @@ class ConsultaAnalisis extends Component {
         )
     }
 
+    patientRender(){
+        return(
+            <div>
+                <Header>Paciente</Header>
+                    <Form.Field label='Id Análisis' value={this.state.analisis.analisisId} control='input' />
+                    <Form.Field label='Fecha Creación' value={this.state.analisis ? getHumanDate(this.state.analisis.createdAt) : ''} control='input' />
+                    <Form.Field label='Estado' value={this.state.analisis ? this.state.analisis.estadoAnalisis.nombre : ''} control='input' />
+                    <Form.Input label='Nombre' iconPosition='left' value={this.state.analisis !== undefined ? this.state.analisis.paciente.nombre + ' ' + checkAtributo(this.state.analisis.paciente.apellido) : ''}>
+                        <Icon name={this.getIconTipo(this.state.analisis ? this.state.analisis.paciente.type : '')}/>
+                        <input/>
+                    </Form.Input>
+
+                    {this.state.analisis.paciente.type === 'com.leloir.backend.domain.Persona' ? 
+                        <div>
+                            <b>Documento</b>
+                            <Form.Group widths='equal'>
+                                <Form.Field label='Tipo' value={this.state.analisis ? this.state.analisis.paciente.tipoDocumento.nombre : ''} control='input' />  
+                                <Form.Field label='Número' value={this.state.analisis ? this.state.analisis.paciente.nroDocumento : ''} control='input' />  
+                            </Form.Group>
+                            
+                            {(this.state.analisis && this.state.analisis.paciente.obraSocial!==undefined) ?
+                                <div>
+                                    <b>Obra Social</b>
+                                    <Form.Group widths='equal'>
+                                        <Form.Field label='Razón Social' value={this.state.analisis.paciente.obraSocial.razonSocial} control='input' />
+                                        
+                                    {this.state.analisis.paciente.plan!==undefined ?
+                                        <Form.Field label='Plan' value={this.state.analisis.paciente.plan.nombre} control='input' />  
+                                        : <Form.Field label='Plan' value='Sin plan' control='input' />   }
+                                    </Form.Group>
+                                </div>
+                            : <Form.Field label='Obra Social' value='No posee' control='input' /> }
+                        </div>
+                    : null}
+
+                    {this.state.analisis.paciente.type === 'com.leloir.backend.domain.Animal' ? 
+                    <div>
+                        <Form.Field label='Tipo de Animal' value={this.state.analisis ? this.state.analisis.paciente.tipoAnimal.nombre : ''} control='input' />  
+                        <Form.Field label='Propietario' value={this.state.analisis ? this.state.analisis.paciente.propietario : ''} control='input' />  
+                    </div>
+                    : null}
+            </div>
+        )
+    }
+
+    muestrasRender(){
+        return(
+            <div>
+                <Header>Muestras</Header>
+
+                {(this.state.analisis.muestras.length === 0 || this.state.analisis.muestras.length === undefined) ? <div>No existen muestras para este análisis
+                   
+                    {(!this.state.showMuestra && this.state.analisis.estadoAnalisis.nombre !== "ENTREGADO") ?
+                        <Button icon labelPosition='left' primary size='small' onClick={() => this.setState({showMuestra: true})} className='muestras'>
+                            <Icon name='plus' /> Añadir muestra
+                        </Button>
+                    : null }
+                </div>: 
+
+                <Table compact>
+                    <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Id</Table.HeaderCell>
+                        <Table.HeaderCell>Tipo</Table.HeaderCell>
+                        <Table.HeaderCell>Estado</Table.HeaderCell>
+                    </Table.Row>
+                    </Table.Header>
+                    
+                    <Table.Body>
+                    {this.state.analisis.muestras.map(muestra => (
+                    <Table.Row>
+                        <Table.Cell>{muestra.idMuestra}</Table.Cell>
+                        <Table.Cell>{muestra.tipoMuestra.nombre}</Table.Cell>
+                        <Table.Cell>{muestra.estadoMuestra.nombre}</Table.Cell>
+                    </Table.Row>
+                    ))}
+                    </Table.Body>
+
+                    <Table.Footer fullWidth>
+                        <Table.Row>
+                            <Table.HeaderCell />
+                            <Table.HeaderCell colSpan='4'>
+                            {(!this.state.showMuestra && this.state.analisis.estadoAnalisis.nombre !== "ENTREGADO") ?
+                                <Button floated='right' icon labelPosition='left' primary size='small' onClick={() => this.setState({showMuestra: true}) }
+                                >
+                                    <Icon name='plus' /> Añadir muestra
+                                </Button> 
+                            : null }
+                            </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Footer>
+                </Table>
+                }                
+
+                {this.state.showMuestra ? this.createMuestra() : null}
+            </div>
+        )
+    }
+
 
     render() {
         var prevURL = this.props.location.state.prevPath || '/analisis'
         return (
-          <div className='union'>
-            <MenuOpciones/>
+            <div className='union'>
+                <MenuOpciones/>
 
-            {this.state.analisis ?
-            
-            <Form  className="analisisConsulta">
-                <Button className='boton' as= {Link} to={prevURL} exact='true' floated='left' icon labelPosition='left' primary size='small'>
-                    <Icon name='arrow alternate circle left' /> Volver
-                </Button>
-                
-                <br/><br/><br/><br/>
+                {this.state.analisis ?
+                    <Form  className='formConsultaAnalisis'>
+                        <Button className='boton' as= {Link} to={prevURL} exact='true' floated='left' icon labelPosition='left' primary size='small'>
+                            <Icon name='arrow alternate circle left' /> Volver
+                        </Button>
 
-                <Segment>
-                <Grid width='equal' divided>
-                    <Grid.Column width={5}>
-                    <Header>Datos del análisis</Header>
-                        <Form.Field label='Id Análisis' value={this.state.analisis.analisisId} control='input' />
+                        <Segment>
+                            <Grid width='equal' divided>
+                                <Grid.Column width={5}>
+                                    {this.patientRender()}
 
-                        <Form.Field label='Fecha Creación' value={this.state.analisis ? getHumanDate(this.state.analisis.createdAt) : ''} control='input' />
+                                </Grid.Column>
 
-                        <Form.Field label='Estado' value={this.state.analisis ? this.state.analisis.estadoAnalisis.nombre : ''} control='input' />
+                                <Grid.Column width={5} divided>
+                                    <Header>Determinaciones</Header>
+                                    {this.renderCard()}
+                                    {this.handleModalContent()}
+                                    
+                                </Grid.Column>
 
-                        <Form.Input label='Nombre' iconPosition='left' value={this.state.analisis !== undefined ? this.state.analisis.paciente.nombre + ' ' + checkAtributo(this.state.analisis.paciente.apellido) : ''}>
-                            <Icon name={this.getIconTipo(this.state.analisis ? this.state.analisis.paciente.type : '')}/>
-                            <input/>
-                        </Form.Input>
-
-                        {this.state.analisis.paciente.type === 'com.leloir.backend.domain.Persona' ? 
-                        <Form.Group widths='equal'>
-                            <Form.Field label='Tipo de Documento' value={this.state.analisis ? this.state.analisis.paciente.tipoDocumento.nombre : ''} control='input' />  
-                            <Form.Field label='Número' value={this.state.analisis ? this.state.analisis.paciente.nroDocumento : ''} control='input' />  
-                        </Form.Group>
-                        : null}
-
-                        {this.state.analisis.paciente.type === 'com.leloir.backend.domain.Animal' ? 
-                        <div>
-                            <Form.Field label='Tipo de Animal' value={this.state.analisis ? this.state.analisis.paciente.tipoAnimal.nombre : ''} control='input' />  
-                            <Form.Field label='Propietario' value={this.state.analisis ? this.state.analisis.paciente.propietario : ''} control='input' />  
-                        </div>
-                        : null}
-
-                    </Grid.Column>
-
-                    <Grid.Column width={5} divided>
-                        <Header>Determinaciones</Header>
-                        {this.renderCard()}
-                        {this.handleModalContent()}
-                          
-                    </Grid.Column>
-
-                    <Grid.Column width={6} divided>
-                    <Header>Muestras</Header>
-
-                        {this.state.analisis.muestras.length === 0 ? <div>No existen muestras para este análisis
-                            <br/><br/>
-                            <Button icon labelPosition='left' primary size='small' onClick={() => this.setState({showModal: true}) }
-                            >
-                                <Icon name='plus' /> Añadir muestra
-                            </Button>
-                        </div>: 
-
-                        <Table compact>
-                            <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell>Id</Table.HeaderCell>
-                                <Table.HeaderCell>Tipo</Table.HeaderCell>
-                                <Table.HeaderCell>Estado</Table.HeaderCell>
-                            </Table.Row>
-                            </Table.Header>
+                                <Grid.Column width={6} divided>
+                                    {this.muestrasRender()}
+                        
+                                </Grid.Column>
                             
-                            <Table.Body>
-                            {this.state.analisis.muestras.map(muestra => (
-                            <Table.Row>
-                                <Table.Cell>{muestra.idMuestra}</Table.Cell>
-                                <Table.Cell>{muestra.tipoMuestra.nombre}</Table.Cell>
-                                <Table.Cell>{muestra.estadoMuestra.nombre}</Table.Cell>
-                            </Table.Row>
-                            ))}
-                            </Table.Body>
-
-                            <Table.Footer fullWidth>
-                                <Table.Row>
-                                    <Table.HeaderCell />
-                                    <Table.HeaderCell colSpan='4'>
-                                    <Button floated='right' icon labelPosition='left' primary size='small' onClick={() => this.setState({showModal: true}) }
-                                    >
-                                        <Icon name='plus' /> Añadir muestra
-                                    </Button>
-                                    </Table.HeaderCell>
-                                </Table.Row>
-                            </Table.Footer>
-                        </Table>
-                        }                
-
-                        <br/><br/>
-                        {this.state.showModal ? this.createMuestra() : null}
-                        <br/><br/>
-                    </Grid.Column>
-                
-                </Grid>
-                
-                </Segment>
-                
-                <br/>
-                
-            </Form>
-            : null }
+                            </Grid>
+                        </Segment>
+                        
+                    </Form>
+                : null }
             }
      
           </div>
-        )
-    }
+    )
+}
 
     emitirAnalisis = () => {
         axios.get(urlEmitirAnalisis + this.state.idAnalisis, {responseType: 'blob',}).then((response) => {
