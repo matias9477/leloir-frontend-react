e1f6007b10124d378759ab664deef27dba031ea3
Bruno Valdeiglesias <bruno.valdeiglesias@gmail.com>
2020-05-21 16:47:14 -0300

    Test trigger deploy en ec2 2

--- deploy-script.sh
+++ deploy-script.sh
@@ -2,7 +2,7 @@
 mkdir -p /home/ubuntu/logs
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/home/ubuntu/leloir_logs/"$(date -d "today" +"%Y%m%d%H%M")"-log.out 2>&1
+exec 1>/home/ubuntu/logs/"$(date -d "today" +"%Y%m%d%H%M")"-log.out 2>&1
 # Everything below will go to the log file:
 
 echo "$(date) : FRONTEND deploy triggered."
