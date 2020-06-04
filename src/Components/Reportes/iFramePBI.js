810061f7fe5ae03aa22b76c2eb4f4cc6b9d8ceb9
solmalisani <solmalisani@gmail.com>
2020-06-04 13:55:55 -0300

    fix css reporte con menu nuevo

--- src/Components/Reportes/iFramePBI.js
+++ src/Components/Reportes/iFramePBI.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import NavBar from '../NavBar/NavBar'
+import NavBar from '../NavBar/NavBar';
 import '../styles.css';
 
 class iFramePBI extends Component {
     render() {
         return (
-            <div className="union">
+            <div>
                 <NavBar/>
-                <div className="avoidMenu">
-                    <iframe width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiMzFhMTdlMmEtZTQxMC00NjBlLTkwZGItNWI4ZTFmOGU4YTk5IiwidCI6ImI5NzNlNjljLTYzYzItNGY0MS1iMzU2LTgyZDY5Yjc4NjRjMiJ9" frameborder="0" allowFullScreen="true"></iframe>              
+                <div className='avoidMenu'>
+                    <iframe width='933' height='700' src='https://app.powerbi.com/view?r=eyJrIjoiMzFhMTdlMmEtZTQxMC00NjBlLTkwZGItNWI4ZTFmOGU4YTk5IiwidCI6ImI5NzNlNjljLTYzYzItNGY0MS1iMzU2LTgyZDY5Yjc4NjRjMiJ9' frameborder='0' allowFullScreen='true'></iframe>              
                 </div>
             </div>
         );
