3b5f21ccfe703365baf0997b387551085da6f7a2
matias <matias.turra@gmail.com>
2020-07-27 14:38:08 -0300

    Shallow testing Tabla with mocked props SUCCESSFUL

--- src/Components/Reusables/Tabla/Tabla.test.js
+++ src/Components/Reusables/Tabla/Tabla.test.js
@@ -3,7 +3,29 @@ import { shallow } from 'enzyme';
 
 import Tabla from './Tabla';
 
-const pacientes = {}
+const pacientes = []
+
+const columns = [
+    {
+        dataField: 'fechaAlta',
+        text: 'Fecha Alta',
+    },
+    {
+        dataField: 'tipoPaciente',
+        text: 'Tipo',
+        type: 'icon',
+    },
+    {
+        dataField: 'nombre',
+        text: 'Paciente',
+    },
+    {
+        dataField: 'nroDocumento',
+        text: 'Documento',
+    }
+];
+
+
 
 it('should render the component correctly with the given mocked props', ()=>{
     const component = shallow(<Tabla
