9051242a9baeb10a6fd11d696b09a8ff87055b4f
matias <matias.turra@gmail.com>
2020-12-06 12:08:44 -0300

    Agregado duck, store entry y componente de charts conectado, cuando esté lista la url para pegarle al servicio ta ready

--- src/Components/LandingPages/Charts.js
+++ src/Components/LandingPages/Charts.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { PieChart, Pie, Tooltip } from 'recharts';
-import './LandingPages.css';
+import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react'
+import { PieChart, Pie, Tooltip } from 'recharts';
+import ClipLoader from 'react-spinners/ClipLoader';
+import { connect } from 'react-redux';
 
+import { getRankingAction } from '../../Redux/reportesDuck';
+import './LandingPages.css';
 
-const Charts = () => {
 
     const data =[
         {name: "Analisis de orina", value: 200},
@@ -14,15 +16,41 @@ const Charts = () => {
         {name: "analisis generico 2", value:12},
     ]
 
-    return (
+class Charts extends Component {
+
+    componentDidMount() {
+        this.props.getRankingAction();
+    }
+
+    render(){
+        const { fetching } = this.props;
+        return (
         <div className="pie-charts">
             <Header as='h3' dividing>Gráficos estadísticos de determinaciones mas pedidas</Header>
-                <PieChart width={400} height={400}>
-                    <Pie dataKey="value" isAnimationActive={true} data={data} cx={200} cy={200} outerRadius={80} fill="#067ebf" label />
-                    <Tooltip />
-                </PieChart>
+            {   fetching ? 
+                    <div className='spinner'>
+                        <ClipLoader
+                            size={60}
+                            color={'black'}/>
+                    </div>
+                    :
+                    
+                    <div>
+                        <PieChart width={400} height={400}>
+                            <Pie dataKey="value" isAnimationActive={true} data={data} cx={200} cy={200} outerRadius={80} fill="#067ebf" label />
+                            <Tooltip />
+                        </PieChart>
+                    </div>                
+            }
+            
         </div>
-    )
+        )
+}
 }
 
-export default Charts;
+const mapStateToProps = (state) => ({
+    ranking: state.reportes.ranking,
+    fetching: state.reportes.fetching,
+})
+
+export default connect(mapStateToProps, { getRankingAction })(Charts);
---
+++ src/Redux/reportesDuck.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+let urlRanking = 'acá va la URL para pegarle al servicio de python'
+
+let initialData = {
+    fetching: false,
+    ranking: [],
+}
+
+
+let GET_RANKING = 'GET_RANKING';
+let GET_RANKING_SUCCESS = 'GET_RANKING_SUCCESS';
+let GET_RANKING_ERROR = 'GET_RANKING_ERROR';
+
+export default function reducer(state = initialData, action){
+    switch(action.type){
+        case GET_RANKING:
+            return {...state, fetching: true}
+        case GET_RANKING_ERROR:
+            return {...state, fetching: false, error: action.payload}
+        case GET_RANKING_SUCCESS:
+            return {...state, fetching: false, ranking: action.payload}            
+    }
+}
+
+export let getRankingAction = () => ( dispatch ) =>{
+    dispatch({
+        type: GET_RANKING
+    })
+    return axios.get(`${urlRanking}`)
+    .then( res => {
+        dispatch({
+            type: GET_RANKING_SUCCESS,
+            payload: Object.values(res.data).flat(),
+        })
+    }).catch(err=>{
+        dispatch({
+            type: GET_RANKING_ERROR,
+            payload: err.message
+        })
+    })
+}
