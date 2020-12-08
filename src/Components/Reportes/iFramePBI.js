4d62b8a5e20cf73a1b16113d96a2c0e50cff21c1
Matias Mercado <matymer94@gmail.com>
2020-12-08 23:40:55 +0000

    iFramePBI.js edited online with Bitbucket

--- src/Components/Reportes/iFramePBI.js
+++ src/Components/Reportes/iFramePBI.js
@@ -8,7 +8,7 @@ class iFramePBI extends Component {
             <div>
                 <NavBar/>
                 <div className='avoidMenu'>
-                    <iframe width="1140" height="700" src="https://app.powerbi.com/reportEmbed?reportId=444126c9-dfb9-4f35-8a86-164e7806a845&autoAuth=true&ctid=82dcaed7-6bb5-4354-8298-b43eb4e0e4ca&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXVzLWVhc3QyLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>              
+                    <iframe title="Data" width="1140" height="700" src="https://app.powerbi.com/reportEmbed?reportId=444126c9-dfb9-4f35-8a86-164e7806a845&autoAuth=true&ctid=82dcaed7-6bb5-4354-8298-b43eb4e0e4ca&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXVzLWVhc3QyLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>              
                 </div>
             </div>
         );
